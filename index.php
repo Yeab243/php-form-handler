d6ccfd75a82349560d3c2955977643fa3b6c62c0
Yeab243 <yeabsira.girma@seattlecolleges.edu>
2022-06-02 22:55:01 -0700

    Removes form.php in favor of index.php

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
-<html>
-  <head>
-    <title>Form Handler</title>
-  </head>
-  <body>
-    <h1>Form Handler</h1>
-    
-    <?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
 
-    if(isset($_POST["FirstName"])){//show data
-      echo $_POST["FirstName"];
-    }else{//show form
-      echo '
-          <form method="post" action=""> 
-            <p>First Name: <input type="text" name="FirstName"/> </p>
-            <p> <input type="submit"/></p>
-          </form>
+<div class="container mt-3">
+  <h2>Stacked form</h2>
+  <form action="/action_page.php">
+    <div class="mb-3 mt-3">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="mb-3">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    </div>
+    <div class="form-check mb-3">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
 
-           ';   
-    }
-    ?>
-    
-    
-    
-    
-    
-  </body>
+</body>
 </html>
